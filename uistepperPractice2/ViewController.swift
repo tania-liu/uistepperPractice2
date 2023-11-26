9727c5e5285181739720c0ab5202217eb6d7266d
Tania Liu <tanialiu430006@gmail.com>
2023-11-26 20:52:16 +0800

    Initial Commit

---
+++ uistepperPractice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uistepperPractice2
+//
+//  Created by 劉俐廷 on 2023/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
